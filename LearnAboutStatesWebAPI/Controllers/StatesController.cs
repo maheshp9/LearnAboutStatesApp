731e8650a54fbf39848f8d5ae09d64d75057a78b
maheshp9 <mahesh.pasupuleti@gmail.com>
2015-05-10 18:54:46 -0700

    initial stateInfo

---
+++ LearnAboutStatesWebAPI/Controllers/StatesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LearnAboutStatesWebAPI.Models;
+
+namespace LearnAboutStatesWebAPI.Controllers
+{
+    public class StatesController : ApiController
+    {
+        private State[] states = new State[]
+        {
+            new State
+            {
+                Code = "CA",
+                Description = "California State",
+                MapUri = @"http://en.wikipedia.org/wiki/California",
+                Name = "California"
+            },
+            new State()
+            {
+                Code = "WA",
+                Description = "Washington State",
+                MapUri = @"http://en.wikipedia.org/wiki/Washington_(state)",
+                Name = "Washington"
+            }
+        };
+
+        public IEnumerable<State> GetAllStates()
+        {
+            return states;
+        }
+
+        public IHttpActionResult GetState(string code)
+        {
+            var state = states.FirstOrDefault((s) => s.Code == code);
+            if (state == null)
+            {
+                return NotFound();
+            }
+            return Ok(state);
+        }
+
+    }
+}
---
+++ LearnAboutStatesWebAPI/Models/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnAboutStatesWebAPI.Models
+{
+    public class State
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string MapUri { get; set; }
+
+        public string Description { get; set; }
+    }
+}
